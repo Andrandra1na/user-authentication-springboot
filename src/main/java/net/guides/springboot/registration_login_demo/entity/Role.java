fad55e02413e5aab1f42284faa5bf397b26e211d
Andrandraina <se20200047@gmail.com>
2025-04-28 09:12:40 +0300

    🛠 Création des entités User et Role pour la gestion des utilisateurs

---
+++ src/main/java/net/guides/springboot/registration_login_demo/entity/Role.java
@@ -0,0 +1,26 @@
+package net.guides.springboot.registration_login_demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="roles")
+public class Role
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false, unique=true)
+    private String name;
+
+    @ManyToMany(mappedBy="roles")
+    private List<User> users;
+}
